f0027e82f5a3f8c9e9ff8413bf415aa54dd5d5e8
1620165757 <1620165757@qq.com>
2018-07-10 00:35:03 +0800

    aa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml'
+import printMe from './print.js'
 function component() {
     let element = document.createElement('div');
-
-    //Lodash, now imported by this script
+    let btn = document.createElement('button')
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    let myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-    console.log(Data);
+    element.appendChild(btn);
     return element;
 }
 
