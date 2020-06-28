e20810b25a842dd65d977f71ff19d0f43498bc5c
zxs <1620165757@qq.com>
2020-06-28 09:30:38 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './style.css';
+import {cube} from './math.js';
+
+function component() {
+
+  if (process.env.NODE_ENV === 'production') {
+    console.log('生产环境！！！');
+  }
+
+  var element = document.createElement('pre');
+
+  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+  })
+}
