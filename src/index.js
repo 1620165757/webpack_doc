5564bec45fafbf54e2b3d7584f343e50ce399567
zxs <1620165757@qq.com>
2020-07-09 11:33:35 +0800

    代码分离

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ function component() {
   let br = document.createElement('br');
 
   button.innerHTML = 'Click me and look at the console!';
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.appendChild(br);
   element.appendChild(button);
 
   // Note that because a network request is involved, some indication
   // of loading would need to be shown in a production-level site/app.
-  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    let print = module.default;
-    print();
-  });
+  // button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+  //   let print = module.default;
+  //   print();
+  // });
 
   return element;
 }
