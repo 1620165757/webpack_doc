06ff40808075dd144eb5af69cf12c50ba15b8650
zxs <1620165757@qq.com>
2020-07-20 11:53:23 +0800

    ts

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-console.log('serviceWorker','serviceWorker' in navigator);
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
-
 function component() {
   var btn = document.createElement('button');
 
   btn.innerHTML = 'stop work';
-  btn.onclick = function () {
-    navigator.serviceWorker.getRegistration('/service-worker.js').then(registration => {
-      registration.unregister();
-    });
-  };
 
   return btn;
 }
