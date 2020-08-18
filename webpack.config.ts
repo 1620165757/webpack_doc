8e32dc4156a14ecb3ecb25e6910260bb32cd6167
zxs <1620165757@qq.com>
2020-08-18 14:35:21 +0800

    入口

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,18 +6,15 @@ import * as webpack from 'webpack';
 const config: webpack.Configuration = {
   //绝对路径，可选，默认使用当前目录，指明entry入口的路径
   context: path.resolve('./src'),
-
   //可以为字符串，对象，数组，方法
   //字符串，chunk命名为main.bundle.js
   //数组，chunk命名为main.bundle.js
   //对象，chunk命名为[key].bundle.js
   entry: './index.js',
-
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       title: '渐进式网络应用程序'
